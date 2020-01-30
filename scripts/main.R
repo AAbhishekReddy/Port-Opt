41d507c95300fe4ae6001ecf9ee148b566297874
AAbhishekReddy <annemabhishekreddy@gmail.com>
2020-01-30 08:14:29 +0530

    Risk free rate added

--- scripts/main.R
+++ scripts/main.R
@@ -5,7 +5,6 @@ library(ggplot2)
 
 sharpe_ratio = function(x) { 
   return (mean(portfolio_returns(x))/sqrt(var(portfolio_returns(x))))
-  
 }
 
 obj  = function(x) {
@@ -25,19 +24,17 @@ penalty = function(x) {
 
 portfolio_returns = function(x) {
   port.returns = 0
-  
   for (i in 1:length(x)) {
-    port.returns = port.returns + profit[,i] * x[i]
+    port.returns = port.returns + profit[,i] * x[i] - profit[,8]
   }
-  
+  # print(length(port.returns))
   return (port.returns)
 }
 
 
-#setwd("/home/ghost/Desktop/Port-Opt/new_york")
+#setwd("/home/ghost/Desktop/Port-Opt/new_york/Data")
 #csv_files = c("AAPL.csv","TSLA.csv","AMZN.csv","GOOG.csv","NFLX.csv","FB.csv")
-csv_files = c("A.csv","AAL.csv","AAP.csv","AAPL.csv","ABC.csv","ABT.csv")
-#csv_files = c("a.csv", "aal.csv", "aap.csv", "aapl.csv", "abbv.csv", "abc.csv", "abt.csv", "acn.csv", "adbe.csv", "adi.csv", "adm.csv", "adp.csv", "ads.csv", "adsk.csv", "aee.csv", "aep.csv", "aes.csv", "aet.csv", "afl.csv", "agn.csv", "aig.csv", "aiv.csv", "aiz.csv", "ajg.csv", "akam.csv", "alb.csv", "alk.csv", "all.csv", "alle.csv", "alxn.csv", "amat.csv", "ame.csv", "amg.csv", "amgn.csv", "amp.csv", "amt.csv", "amzn.csv", "an.csv", "antm.csv", "aon.csv", "apa.csv", "apc.csv", "apd.csv", "aph.csv", "arnc.csv", "atvi.csv", "avb.csv", "avgo.csv", "avy.csv", "awk.csv", "axp.csv", "ayi.csv", "azo.csv", "ba.csv", "bac.csv", "bax.csv", "bbby.csv", "bbt.csv", "bby.csv", "bcr.csv", "bdx.csv", "ben.csv", "bhi.csv", "biib.csv", "bk.csv", "blk.csv", "bll.csv", "bmy.csv", "bsx.csv", "bwa.csv", "bxp.csv", "c.csv", "ca.csv", "cag.csv", "cah.csv", "cat.csv", "cb.csv", "cbg.csv", "cbs.csv", "cci.csv", "ccl.csv", "celg.csv", "cern.csv", "cf.csv", "cfg.csv", "chd.csv", "chk.csv", "chrw.csv", "chtr.csv", "ci.csv", "cinf.csv", "cl.csv", "clx.csv", "cma.csv", "cmcsa.csv", "cme.csv", "cmg.csv", "cmi.csv", "cms.csv", "cnc.csv", "cnp.csv", "cof.csv", "cog.csv", "coh.csv", "col.csv", "coo.csv", "cop.csv", "cost.csv", "coty.csv", "cpb.csv", "crm.csv", "csco.csv", "csra.csv", "csx.csv", "ctas.csv", "ctl.csv", "ctsh.csv", "ctxs.csv", "cvs.csv", "cvx.csv", "cxo.csv", "d.csv", "dal.csv", "dd.csv", "de.csv", "dfs.csv", "dg.csv", "dgx.csv", "dhi.csv", "dhr.csv", "dis.csv", "disca.csv", "disck.csv", "dlph.csv", "dlr.csv", "dltr.csv", "dnb.csv", "dov.csv", "dow.csv", "dps.csv", "dri.csv", "dte.csv", "duk.csv", "dva.csv", "dvn.csv", "ea.csv", "ebay.csv", "ecl.csv", "ed.csv", "efx.csv", "eix.csv", "el.csv", "emn.csv", "emr.csv", "endp.csv", "eog.csv", "eqix.csv", "eqr.csv", "eqt.csv", "es.csv", "esrx.csv", "ess.csv", "etfc.csv", "etn.csv", "etr.csv", "evhc.csv", "ew.csv", "exc.csv", "expd.csv", "expe.csv", "exr.csv", "f.csv", "fast.csv", "fb.csv", "fbhs.csv", "fcx.csv", "fdx.csv", "fe.csv", "ffiv.csv", "fis.csv", "fisv.csv", "fitb.csv", "fl.csv", "flir.csv", "flr.csv", "fls.csv", "fmc.csv", "fox.csv", "foxa.csv", "frt.csv", "fslr.csv", "fti.csv", "ftr.csv", "ftv.csv", "gd.csv", "ge.csv", "ggp.csv", "gild.csv", "gis.csv", "glw.csv", "gm.csv", "goog.csv", "googl.csv", "gpc.csv", "gpn.csv", "gps.csv", "grmn.csv", "gs.csv", "gt.csv", "gww.csv", "hal.csv", "har.csv", "has.csv", "hban.csv", "hbi.csv", "hca.csv", "hcn.csv", "hcp.csv", "hd.csv", "hes.csv", "hig.csv", "hog.csv", "holx.csv", "hon.csv", "hp.csv", "hpe.csv", "hpq.csv", "hrb.csv", "hrl.csv", "hrs.csv", "hsic.csv", "hst.csv", "hsy.csv", "hum.csv", "ibm.csv", "ice.csv", "idxx.csv", "iff.csv", "ilmn.csv", "intc.csv", "intu.csv", "ip.csv", "ipg.csv", "ir.csv", "irm.csv", "isrg.csv", "itw.csv", "ivz.csv", "jbht.csv", "jci.csv", "jec.csv", "jnj.csv", "jnpr.csv", "jpm.csv", "jwn.csv", "k.csv", "key.csv", "khc.csv", "kim.csv", "klac.csv", "kmb.csv", "kmi.csv", "kmx.csv", "ko.csv", "kors.csv", "kr.csv", "kss.csv", "ksu.csv", "l.csv", "lb.csv", "leg.csv", "len.csv", "lh.csv", "lkq.csv", "lll.csv", "lltc.csv", "lly.csv", "lmt.csv", "lnc.csv", "lnt.csv", "low.csv", "lrcx.csv", "luk.csv", "luv.csv", "lvlt.csv", "lyb.csv", "m.csv", "ma.csv", "maa.csv", "mac.csv", "mar.csv", "mas.csv", "mat.csv", "mcd.csv", "mchp.csv", "mck.csv", "mco.csv", "mdlz.csv", "mdt.csv", "met.csv", "mhk.csv", "mjn.csv", "mkc.csv", "mlm.csv", "mmc.csv", "mmm.csv", "mnk.csv", "mnst.csv", "mo.csv", "mon.csv", "mos.csv", "mpc.csv", "mrk.csv", "mro.csv", "msft.csv", "msi.csv", "mtb.csv", "mtd.csv", "mu.csv", "mur.csv", "myl.csv", "navi.csv", "nbl.csv", "ndaq.csv", "nee.csv", "nem.csv", "nflx.csv", "nfx.csv", "ni.csv", "nke.csv", "nlsn.csv", "noc.csv", "nov.csv", "nrg.csv", "nsc.csv", "ntap.csv", "ntrs.csv", "nue.csv", "nvda.csv", "nwl.csv", "nws.csv", "nwsa.csv", "o.csv", "oke.csv", "omc.csv", "orcl.csv", "orly.csv", "oxy.csv", "payx.csv", "pbct.csv", "pbi.csv", "pcar.csv", "pcg.csv", "pcln.csv", "pdco.csv", "peg.csv", "pep.csv", "pfe.csv", "pfg.csv", "pg.csv", "pgr.csv", "ph.csv", "phm.csv", "pki.csv", "pld.csv", "pm.csv", "pnc.csv", "pnr.csv", "pnw.csv", "ppg.csv", "ppl.csv", "prgo.csv", "pru.csv", "psa.csv", "psx.csv", "pvh.csv", "pwr.csv", "px.csv", "pxd.csv", "pypl.csv", "qcom.csv", "qrvo.csv", "r.csv", "rai.csv", "rcl.csv", "regn.csv", "rf.csv", "rhi.csv", "rht.csv", "rig.csv", "rl.csv", "rok.csv", "rop.csv", "rost.csv", "rrc.csv", "rsg.csv", "rtn.csv", "sbux.csv", "scg.csv", "schw.csv", "se.csv", "see.csv", "shw.csv", "sig.csv", "sjm.csv", "slb.csv", "slg.csv", "sna.csv", "sni.csv", "so.csv", "spg.csv", "spgi.csv", "spls.csv", "srcl.csv", "sre.csv", "sti.csv", "stt.csv", "stx.csv", "stz.csv", "swk.csv", "swks.csv", "swn.csv", "syf.csv", "syk.csv", "symc.csv", "syy.csv", "t.csv", "tap.csv", "tdc.csv", "tdg.csv", "tel.csv", "tgna.csv", "tgt.csv", "tif.csv", "tjx.csv", "tmk.csv", "tmo.csv", "trip.csv", "trow.csv", "trv.csv", "tsco.csv", "tsn.csv", "tso.csv", "tss.csv", "twx.csv", "txn.csv", "txt.csv", "uaa.csv", "ual.csv", "udr.csv", "uhs.csv", "ulta.csv", "unh.csv", "unm.csv", "unp.csv", "ups.csv", "urbn.csv", "uri.csv", "usb.csv", "utx.csv", "v.csv", "var.csv", "vfc.csv", "viab.csv", "vlo.csv", "vmc.csv", "vno.csv", "vrsk.csv", "vrsn.csv", "vrtx.csv", "vtr.csv.csv", "vz.csv.csv", "wat.csv.csv", "wba.csv.csv", "wdc.csv.csv", "wec.csv.csv", "wfc.csv.csv", "wfm.csv", "whr.csv", "wltw.csv", "wm.csv", "wmb.csv", "wmt.csv", "wrk.csv", "wu.csv", "wy.csv", "wyn.csv", "wynn.csv", "xec.csv", "xel.csv", "xl.csv", "xlnx.csv", "xom.csv", "xray.csv", "xrx.csv", "xyl.csv", "yhoo.csv", "yum.csv", "zbh.csv", "zion.csv", "zts.csv")
+csv_files = c("a.csv","aal.csv","aap.csv","aapl.csv","abc.csv","abt.csv")
 merged_file = NULL
 
 n = length(csv_files)
@@ -52,6 +49,11 @@ for (i in 1:n) {
 
 write.csv(merged_file, file = "merged-file.csv")
 
+# Reading new merged file with risk free rate
+new_merged <- read.csv("new_merged.csv")
+# merged_file <- read.csv("merged-file")
+
+
 n = ncol(merged_file)
 for (i in 2:n) {
   stock_prices = merged_file[,i] 
@@ -65,6 +67,8 @@ for (i in 2:n) {
 
 profit = merged_file[2:nrow(merged_file),2:ncol(merged_file)]
 
+write.csv(profit, "profit.csv")
+profit <- read.csv("profit.csv")
 
 # Without local search
 ga_res = ga(
@@ -98,7 +102,7 @@ new_ga = ga(
             optimArgs = list(method = "L-BFGS-B", 
                              poptim = 0.05,
                              pressel = 0.5,
-                             control = list(fnscale = -1, maxit = 100)),
+                             control = list(fnscale = -1, maxit = 1000)),
             keepBest = FALSE,
             parallel = TRUE,
             monitor = if(interactive()) gaMonitor else FALSE,
